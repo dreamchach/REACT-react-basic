fcfa8719dbcfcad4e8ec6a09003042b9b0150c12
Kim JiYeong <zmswkfl@naver.com>
2022-12-08 16:48:11 +0900

    title : make component for use react

--- kim-dream/src/index.js
+++ kim-dream/src/index.js
@@ -7,17 +7,16 @@ import reportWebVitals from './reportWebVitals';
 // 작성
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock'
+import CommentList from './chapter_05/CommentList';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-  setInterval(()=>{    
+// root.render(  
     ReactDOM.render(
       <React.StrictMode>
-        <Clock />
+        <CommentList/>
       </React.StrictMode>,
       document.getElementById('root')
     );
-    }, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
