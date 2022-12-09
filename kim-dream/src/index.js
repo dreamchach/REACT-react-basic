7b7d969c391212344c7efe3bd0e93dfc61ae147a
Kim JiYeong <zmswkfl@naver.com>
2022-12-09 16:41:54 +0900

    title : basic html-js react

--- kim-dream/src/index.js
+++ kim-dream/src/index.js
@@ -10,12 +10,19 @@ import Clock from './chapter_04/Clock'
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
+import ProfileCard from './chapter_13/profileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(  
     ReactDOM.render(
       <React.StrictMode>
-        <Accommodate/>
+        <DarkOrLight/>
       </React.StrictMode>,
       document.getElementById('root')
     );
